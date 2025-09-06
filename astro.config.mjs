dd1597a18c86dd09172eb821397585199fb26291
max <ali.mousa.omran+github@gmail.com>
2025-09-07 00:35:51 +0300

    updating sitemap-index.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://kaleem.dev/",
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ar"],
